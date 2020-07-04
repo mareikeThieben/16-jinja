6c946d8327824d94f31207bc0340bdc7f6a4c39f
Mareike Thieben <mthieben@posteo.de>
2020-07-04 20:26:12 +0200

    Fehler ausgebessert

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/about.html")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-if__name__ == '__main__':
+if __name__ == '__main__':
     app.run()
 
 
